2b539050a58233bfd2f3d5a759e07533ac432237
System Administrator <root@YALINde-MacBook-Pro.local>
2019-09-15 15:58:15 +0900

    Updates

---
+++ precache-manifest.0bdeb7fbd7264249f1ba5fd0fce7575b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a88b210081dc82815b5765ce3f82daa",
+    "url": "/eventPage/index.html"
+  },
+  {
+    "revision": "8725eeadd0c16de3c4b2",
+    "url": "/eventPage/static/css/2.7938c64a.chunk.css"
+  },
+  {
+    "revision": "eea17cd2f90406722f46",
+    "url": "/eventPage/static/css/main.7ef6e577.chunk.css"
+  },
+  {
+    "revision": "8725eeadd0c16de3c4b2",
+    "url": "/eventPage/static/js/2.b6d83b1f.chunk.js"
+  },
+  {
+    "revision": "eea17cd2f90406722f46",
+    "url": "/eventPage/static/js/main.112ce2d3.chunk.js"
+  },
+  {
+    "revision": "25756858ce0b16387ba8",
+    "url": "/eventPage/static/js/runtime~main.af8ffd44.js"
+  },
+  {
+    "revision": "3047c76a3d82648123663e3b5472e08a",
+    "url": "/eventPage/static/media/AKB48_Flag_TeamK.3047c76a.png"
+  }
+]);
